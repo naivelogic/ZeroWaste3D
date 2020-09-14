fa06f713116e9d49d4bfb0c8a44b0d2a9f95a05b
naivelogic <threetimberland@gmail.com>
2020-09-14 14:09:00 +0000

    coffeecup ply + 3D notebooks

---
+++ research/6D_Pose/notebooks/notebook_tools.py
@@ -0,0 +1,34 @@
+import json
+import numpy as np
+
+def load_params_from_json(json_path):
+    with open(json_path, 'r') as f:
+        return json.load(f)
+
+def load_mesh_files(mesh_path):
+    with open(mesh_path, 'r') as file:
+        _ = file.readlines()
+        mesh_files = [i.rstrip() for i in _]
+    
+    return mesh_files
+
+def get_3D_corners(vertices):
+    
+    min_x = np.min(vertices[0,:])
+    max_x = np.max(vertices[0,:])
+    min_y = np.min(vertices[1,:])
+    max_y = np.max(vertices[1,:])
+    min_z = np.min(vertices[2,:])
+    max_z = np.max(vertices[2,:])
+
+    corners = np.array([[min_x, min_y, min_z],
+                        [min_x, min_y, max_z],
+                        [min_x, max_y, min_z],
+                        [min_x, max_y, max_z],
+                        [max_x, min_y, min_z],
+                        [max_x, min_y, max_z],
+                        [max_x, max_y, min_z],
+                        [max_x, max_y, max_z]])
+
+    corners = np.concatenate((np.transpose(corners), np.ones((1,8)) ), axis=0)
+    return corners
